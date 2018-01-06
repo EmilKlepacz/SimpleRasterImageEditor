2c254fff277336f8380a2534c3f4d379fc141c32
kubaaan <jnyckowiak@gmail.com>
2018-01-06 15:44:22 +0100

    load file from URL + error handling

--- src/main/java/fx/app/controllers/OpenFileController.java
+++ src/main/java/fx/app/controllers/OpenFileController.java
@@ -128,9 +128,9 @@ public class OpenFileController {
         }
     }
 
-    private void handleOpenFromURL() throws IOException {
+    private void handleOpenFromURL(String url) throws IOException {
         File f = new File("temp");
-        String chosenURL = "https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png"; // tu daj zmienną do choosera FX
+        String chosenURL = url;
         BufferedImage img = ImageIO.read(new URL(chosenURL));
         String extension = chosenURL.substring(chosenURL.lastIndexOf(".") + 1).trim();
         ImageIO.write(img, extension, f);
@@ -237,22 +237,27 @@ public class OpenFileController {
     }
 
     public void openUrlWindow() {
-        TextInputDialog dialog = new TextInputDialog("https://");
+        TextInputDialog dialog = new TextInputDialog();
         dialog.setTitle("Load from url");
         dialog.setHeaderText("Look, put your url to file below");
         dialog.setContentText("Please enter file url:");
-
 
         Image image = new Image(getClass().getResource("/images/url.png").toExternalForm());
         ImageView imageView = new ImageView(image);
         dialog.setGraphic(imageView);
 
-
         dialog.setGraphic(imageView);
 
         Optional<String> result = dialog.showAndWait();
         if (result.isPresent()){
-            System.out.println("Your name: " + result.get());
+            try {
+                handleOpenFromURL(dialog.getEditor().getCharacters().toString());
+            } catch (IOException e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setContentText("WRONG URL WAS USED!");
+                alert.showAndWait();
+                openUrlWindow();
+            }
         }
 
     }
