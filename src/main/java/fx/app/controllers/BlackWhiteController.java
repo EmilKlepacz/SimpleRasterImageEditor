c8070a8390298646258c4b702a10b116d86521cd
kubaaan <jnyckowiak@gmail.com>
2018-01-07 16:02:01 +0100

    converting to black and white operation added

--- src/main/java/fx/app/controllers/BlackWhiteController.java
+++ src/main/java/fx/app/controllers/BlackWhiteController.java
@@ -1,9 +1,17 @@
 package fx.app.controllers;
 
 
+import ij.ImagePlus;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class BlackWhiteController extends BasicController {
     @FXML
@@ -20,7 +28,17 @@ public class BlackWhiteController extends BasicController {
         addChangesToHistory(image);
     }
 
-    public void blackAndWhite(){
+    public void blackAndWhite() throws IOException {
+        BufferedImage startingImage;
+        BufferedImage blackWhite;
+        startingImage = ImageIO.read(new File(imagePath));
 
+        blackWhite = new BufferedImage(startingImage.getWidth(), startingImage.getHeight(), BufferedImage.TYPE_BYTE_BINARY);
+        Graphics2D g2d = blackWhite.createGraphics();
+        g2d.drawImage(startingImage, 0, 0, null);
+
+        ImagePlus blackAndWhiteImagePlus = new ImagePlus("gray", blackWhite);
+        Image blackAndWhiteImage = SwingFXUtils.toFXImage(blackAndWhiteImagePlus.getBufferedImage(), null);
+        addChangesToImage(blackAndWhiteImage);
     }
 }
