3737c52441d3c3679294a7444c5cf6f535621f07
kubaaan <jnyckowiak@gmail.com>
2018-01-07 23:40:14 +0100

    Merge branch 'master' of C:\Users\komp1\Documents\GitHub\SimpleRasterImageEditor with conflicts.

--- src/main/java/fx/app/controllers/FilteringController.java
+++ src/main/java/fx/app/controllers/FilteringController.java
@@ -7,9 +7,15 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import marvin.image.MarvinImage;
 
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
+import java.util.Random;
 
 public class FilteringController extends BasicController {
 
@@ -28,7 +34,7 @@ public class FilteringController extends BasicController {
         saveTemporaryFile(imageViewFiltering.getImage());
     }
 
-    public void undoActionForFilterController(){
+    public void undoActionForFilterController() {
         handleUndoAction();
     }
 
@@ -39,7 +45,7 @@ public class FilteringController extends BasicController {
         saveTemporaryFile(imageViewFiltering.getImage());
     }
 
-    public void saveActionForFilteringController(){
+    public void saveActionForFilteringController() {
         handleSaveAction();
     }
 
@@ -47,7 +53,7 @@ public class FilteringController extends BasicController {
     protected void handleSaveAction() {
         try {
             openFileController.addChangesToImage(image);
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -63,7 +69,7 @@ public class FilteringController extends BasicController {
     }
 
     public void setOpenFileController(OpenFileController openFileController) {
-        if(this.openFileController == null)
+        if (this.openFileController == null)
             this.openFileController = openFileController;
     }
 
@@ -77,11 +83,70 @@ public class FilteringController extends BasicController {
         addChangesToImage(negativeImage);
     }
 
+    public void meanFilter() throws IOException {
+
+        BufferedImage img = ImageIO.read(new File(temporaryImagePath));
+
+        //get dimensions
+        int maxHeight = img.getHeight();
+        int maxWidth = img.getWidth();
+
+        //create 2D Array for new picture
+        int pictureFile[][] = new int[maxHeight][maxWidth];
+        for (int i = 0; i < maxHeight; i++) {
+            for (int j = 0; j < maxWidth; j++) {
+                pictureFile[i][j] = img.getRGB(j, i);
+            }
+        }
+
+        int output[][] = new int[maxHeight][maxWidth];
+
+        //Apply Mean Filter
+        for (int v = 1; v < maxHeight; v++) {
+            for (int u = 1; u < maxWidth; u++) {
+                //compute filter result for position (u,v)
+
+                int sum = 0;
+                for (int j = -1; j <= 1; j++) {
+                    for (int i = -1; i <= 1; i++) {
+                        if ((u + (j) >= 0 && v + (i) >= 0 && u + (j) < maxWidth && v + (i) < maxHeight)) {
+                            int p = pictureFile[v+(i)][u+(j)];
+                            sum = sum + p;
+                        }
+                    }
+                }
+
+                int q = (int) (sum /9);
+                output[v][u] = q;
+            }
+        }
+
+        //Turn the 2D array back into an image
+        BufferedImage theImage = new BufferedImage(
+                maxHeight,
+                maxWidth,
+                BufferedImage.TYPE_INT_ARGB);
+        int value;
+        for (int y = 1; y < maxHeight; y++) {
+            for (int x = 1; x < maxWidth; x++) {
+                value = output[y][x];
+                theImage.setRGB(x, y, value);
+            }
+        }
+
+        ImagePlus grayImg = new ImagePlus("mean", theImage);
+        Image negativeImage = SwingFXUtils.toFXImage(grayImg.getBufferedImage(), null);
+        addChangesToImage(negativeImage);
+    }
+
     public void laplacianFilter(){
-        ImagePlus laplacianImage = new ImagePlus(temporaryImagePath);
-        ImageProcessor imageProcessor = laplacianImage.getProcessor();
+        ImagePlus gaussianBlur = new ImagePlus(temporaryImagePath);
+        ImageProcessor imgProcessor = gaussianBlur.getProcessor();
+        imgProcessor.findEdges();
 
 
+        Image negativeImage = SwingFXUtils.toFXImage(gaussianBlur.getBufferedImage(), null);
+        addChangesToImage(negativeImage);
     }
 
     public void setSigmaSliderVisible(){
